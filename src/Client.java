81d2155fbe15c1af6180b08b304e32c206a2f630
Parth-05 <parthmarathe5@gmail.com>
2022-07-15 16:36:18 +0530

    Added Client and wokring on ReadThread Class

---
+++ src/Client.java
@@ -0,0 +1,33 @@
+import java.net.*;
+import java.io.*;
+
+public class Client {
+    private String hostname;
+    private int port;
+    private String userName;
+
+    public Client(String hostname,int port){
+        this.hostname = hostname;
+        this.port = port;
+    }
+
+    public void execute(){
+        try{
+            Socket socket = new Socket(hostname,port);
+            System.out.println("Connected to chat Server");
+
+            new ReadThread(socket, this).start();
+            new WriteThread(socket, this).start();
+
+        }catch(Exception e){
+            System.out.println("Server not Found: " + e.getMessage());
+        }
+    }
+
+    void SetUserName(String userName){
+        this.userName = userName;
+    }
+     String getUserName(){
+        return this.userName = userName;
+     }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Chat App");
+
+        if (args.length <2)
+            return;
+
+        String hostname = args[0];
+        Integer port = Integer.parseInt(args[1]);
+
+        Client cl = new Client(hostname, port);
+        cl.execute();
     }
 }
